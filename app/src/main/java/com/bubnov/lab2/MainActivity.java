cd6ed8467d8c2d0d6c4b94498bf4a2d08989d516
Opilane TTHK <opilane@tthk.ee>
2022-02-11 10:40:26 +0200

    2022-02-10

--- app/src/main/java/com/bubnov/lab2/MainActivity.java
+++ app/src/main/java/com/bubnov/lab2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -11,6 +12,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int clickCounter = 0;
+    long pressStart = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,17 +22,57 @@ public class MainActivity extends AppCompatActivity {
         layout.setOrientation(LinearLayout.VERTICAL);
         for (int i = 1; i <= 10; i++){
             Button button = new Button( this);
-            button.setText("button" +i);
+            String formatted = getString(R.string.nupp, i);
+            button.setText(formatted);
             button.setBackgroundColor(Color.rgb( 0, i*20, 255));
             layout.addView(button);
         }
-   //     setContentView(layout);
-    }
+        //setContentView(layout);
+
+        Button buttonClicker = findViewById(R.id.btnClicker);
+        buttonClicker.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                clickCounter++;
+                buttonClicker.setText("I was clicked " + clickCounter + " times");
+            }
+        });
+
+        Button buttonReset = findViewById(R.id.btnReset);
+        buttonReset.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    pressStart = System.currentTimeMillis();
+                }else if(motionEvent.getAction()==MotionEvent.ACTION_UP){
+                    long duration = System.currentTimeMillis() - pressStart;
+                    TextView text = findViewById(R.id.textView);
+                    text.setText("Button was pressed for " + duration + " ms");
+                    LinearLayout vertical = findViewById(R.id.vertical_layout);
+                    TextView txtDuration = new TextView(getApplicationContext());
+                    txtDuration.setText("Button was pressed for "+duration+" ms");
+                    vertical.addView(txtDuration);
+                }
+                return false;
+            }
+        });
+
+       /* buttonClicker.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View view) {
+                clickCounter--;
+                buttonClicker.setText("I was clicked " + clickCounter + " times");
+                return false;
+            }
+        });*/
+    };
+
 
     public void resetbtn(View view) {
         clickCounter = 0;
-        Button button = findViewById(R.id.button);
+        Button button = findViewById(R.id.btnClicker);
         button.setText("I was clicked " + clickCounter + " times");
+        LinearLayout vertical = findViewById(R.id.vertical_layout);
+        vertical.removeAllViews();
 
     }
 }
